d7e0717badbbc5319aa3aa36b009971648a864f3
Oleh Kushniryk <kushnirykoleh@gmail.com>
2014-12-11 14:20:28 +0200

    Add jps pages for authentication and move it's handlers from UserControllerto AccessController. Create CustomUserDetailsService to add our user object to security context, edit conf. Apply Viktor's changes

--- src/main/java/org/softserveinc/controller/UserController.java
+++ src/main/java/org/softserveinc/controller/UserController.java
@@ -2,12 +2,12 @@ package org.softserveinc.controller;
 
 import org.softserveinc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 
 @Controller
@@ -19,7 +19,6 @@ public class UserController {
 
     @RequestMapping(value = "/userProfile/{username}", method = RequestMethod.GET)
     public String showUserProfile(@PathVariable String username, Model model) {
-
         model.addAttribute(userService.findUserByUsername(username));
         return "userProfile";
     }
@@ -28,5 +27,4 @@ public class UserController {
     public String showUserProfile() {
         return "userProfile";
     }
-
 }
